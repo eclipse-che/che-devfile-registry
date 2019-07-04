bab94a7c57fd5bdf6bd032cfaf12a3c02964b455
nickboldt <nboldt@redhat.com>
2019-07-04 14:02:17 -0400

    replace the need for yq with simple sed; replace need for findutils with simple ls; remove need for builder image
    Change-Id: I428c11af5b9dd707ad1e76ef43b663ce8cc2aade
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012-2018 Red Hat, Inc.
+# Copyright (c) 2018-2019 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -12,31 +12,35 @@ set -e
 
 # Arguments:
 # 1 - folder to search files in
-function buildIndex() {
-    metaInfoFields=('displayName' 'description' 'tags' 'icon' 'globalMemoryLimit')
+if [[ ! $1 ]]; then echo "Error: must specify path to devfiles, eg., $0 /path/to/che-devfile-registry/devfiles"; exit 1; fi
+devfilesDir=$1; devfilesDir=${devfilesDir%/} # trim trailing slash if present
 
-    ## search for all devfiles
-    readarray -d '' arr < <(find "$1" -name 'meta.yaml' -print0)
+metaInfoFields=('displayName' 'description' 'tags' 'icon' 'globalMemoryLimit')
+
+function buildIndex() {
 
     FIRST_LINE=true
     echo "["
 
-    ## now loop through meta.yaml files
-    for i in "${arr[@]}"
-    do
-        if [ "$FIRST_LINE" = true ] ; then
+    ## loop through meta.yaml files
+    for i in $(ls -1 ${devfilesDir}/*/meta.yaml | sort); do
+        if [[ "$FIRST_LINE" = true ]]; then
             echo "{"
             FIRST_LINE=false
         else
             echo ",{"
         fi
 
-        for field in "${metaInfoFields[@]}"
-        do
+        for field in "${metaInfoFields[@]}"; do
             # get value of needed field in json format
             # note that it may have differrent formats: arrays, string, etc.
             # String value contains quotes, e.g. "str"
-            value="$(yq r -j "$i" "$field")"
+            value=$(cat ${i} | grep "$field": | sed -e "s#${field}: ##")
+            # if not an array and not already wrapped in quotes
+            if [[ ${value} != "["*"]" ]] && [[ ${value} != "\""*"\"" ]]; then 
+                # wrap in quotes
+                value="\"${value}\""
+            fi
             echo "  \"$field\":$value,"
         done
 
@@ -47,4 +51,4 @@ function buildIndex() {
     echo "]"
 }
 
-buildIndex devfiles
+buildIndex
