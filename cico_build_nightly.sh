7bb9e7ddb3bf40941a01a5a72534f7a85616bc4c
Angel Misevski <amisevsk@redhat.com>
2019-07-18 18:50:37 -0400

    Build and push patched images *after* logging in
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- cico_build_nightly.sh
+++ cico_build_nightly.sh
@@ -62,14 +62,14 @@ function build_and_push() {
   IMAGE="che-devfile-registry"
   TAG="nightly"
 
-  "${SCRIPT_DIR}"/arbitrary-users-patch/build_images.sh --push
-  echo "CICO: pushed nightly arbitrary-user patched base images"
-
   if [ -n "${QUAY_ECLIPSE_CHE_USERNAME}" ] && [ -n "${QUAY_ECLIPSE_CHE_PASSWORD}" ]; then
     docker login -u "${QUAY_ECLIPSE_CHE_USERNAME}" -p "${QUAY_ECLIPSE_CHE_PASSWORD}" "${REGISTRY}"
   else
     echo "Could not login, missing credentials for pushing to the '${ORGANIZATION}' organization"
   fi
+
+  "${SCRIPT_DIR}"/arbitrary-users-patch/build_images.sh --push
+  echo "CICO: pushed nightly arbitrary-user patched base images"
 
   # Let's build and push images to 'quay.io'
   docker build -t ${IMAGE} -f ${DOCKERFILE} .
