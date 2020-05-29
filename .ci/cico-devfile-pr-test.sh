6698e96e038a56cf90690d51203602548bb745cd
Ihor Okhrimenko <iokhrime@redhat.com>
2020-05-29 16:30:54 +0300

    Next try #6
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/cico-devfile-pr-test.sh
+++ .ci/cico-devfile-pr-test.sh
@@ -49,10 +49,10 @@ ls -al common-qe
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 export SCRIPT_DIR
 
-. "${SCRIPT_DIR}"/common-qe/che-util.sh
-. "${SCRIPT_DIR}"/common-qe/che-cert-generation.sh
-. "${SCRIPT_DIR}"/common-qe/common-util.sh
-. "${SCRIPT_DIR}"/common-qe/installation-util.sh
+. common-qe/che-util.sh
+. common-qe/che-cert-generation.sh
+. common-qe/common-util.sh
+. common-qe/installation-util.sh
 
 . "${SCRIPT_DIR}"/../cico_functions.sh
 
