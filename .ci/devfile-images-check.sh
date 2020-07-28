d4cace2c2e671f19d905c4292e8297148b255bfb
flacatus <flacatus@redhat.com>
2020-07-28 10:30:32 +0200

    Fix shellcheck errors
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/devfile-images-check.sh
+++ .ci/devfile-images-check.sh
@@ -12,30 +12,38 @@
 set -e
 
 # PR_FILES_CHANGED store all Modified/Created files in Pull Request.
-export PR_FILES_CHANGED=$(git --no-pager diff --name-only HEAD $(git merge-base HEAD origin/master))
+PR_FILES_CHANGED=$(git --no-pager diff --name-only HEAD "$(git merge-base HEAD origin/master)")
+export PR_FILES_CHANGED
 
 # filterDevFileYamls function filter yamls from PR into a new array => FILES_CHANGED_ARRAY.
 function filterDevFileYamls() {
-    export SCRIPT=$(readlink -f "$0")
-    export ROOT_DIR=$(dirname $(dirname "$SCRIPT"));
+    local SCRIPT
+    SCRIPT=$(readlink -f "$0")
+
+    local ROOT_DIR
+    ROOT_DIR=$(dirname "$(dirname "$SCRIPT")");
 
     for files in ${PR_FILES_CHANGED}
     do  
         # Filter only files which are devfiles folder and finish with .yaml extension
         if [[ $files =~ ^devfiles.*.yaml$ ]]; then
             echo "[INFO] Added/Changed new devfile in the current PR: ${files}"
-            export FILES_CHANGED_ARRAY+=("${ROOT_DIR}/"$files)
+            FILES_CHANGED_ARRAY+=("${ROOT_DIR}/"$files)
+            export FILES_CHANGED_ARRAY
         fi
     done 
 }
 
 # checkDevFileImages get the container images from changed devfile.yaml files in PR and check if they have digest.
 function checkDevFileImages() {
-    export IMAGES=$(yq -r '..|.image?' "${FILES_CHANGED_ARRAY[@]}" | grep -v "null" | uniq)
+    IMAGES=$(yq -r '..|.image?' "${FILES_CHANGED_ARRAY[@]}" | grep -v "null" | uniq)
+    export IMAGES
 
     for image in ${IMAGES}
     do
-        local DIGEST="$(skopeo inspect --tls-verify=false docker://${image} 2>/dev/null | jq -r '.Digest')"
+
+        local DIGEST
+        DIGEST="$(skopeo inspect --tls-verify=false docker://"${image}" 2>/dev/null | jq -r '.Digest')"
         if [ -z "${DIGEST}" ];
         then
             echo "[ERROR] Image ${image} doesn't contain an valid digest.Digest check script will fail."
