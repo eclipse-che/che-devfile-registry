65e63d31dfb0f8679f2638519a082355b9b7b69b
Sun Tan <sutan@redhat.com>
2021-03-16 19:33:15 +0000

    chore(devfile): Add first dog fooding devfile
    Signed-off-by: Sun Tan <sutan@redhat.com>

---
+++ build/dev/start-devfile-registry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2018-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+DEFAULT_BUILD_DIR="/projects/build"
+BUILD_DIR=${BUILD_DIR:-$DEFAULT_BUILD_DIR}
+
+DEFAULT_REPO_DIR="/projects/che-devfile-registry"
+REPO_DIR=${REPO_DIR:-$DEFAULT_REPO_DIR}
+
+rm -rf /usr/local/apache2/htdocs/devfiles
+rm -rf /usr/local/apache2/htdocs/images/* 
+
+cp -rf ${BUILD_DIR}/devfiles /usr/local/apache2/htdocs/devfiles
+cp -rf ${REPO_DIR}/images/* /usr/local/apache2/htdocs/images
+export CHE_DEVFILE_REGISTRY_URL=$(cat ${BUILD_DIR}/ENV_CHE_DEVFILE_REGISTRY_URL)
+echo $CHE_DEVFILE_REGISTRY_URL
+/projects/che-devfile-registry/build/dockerfiles/entrypoint.sh httpd-foreground
