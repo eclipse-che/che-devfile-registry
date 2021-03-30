3274a200943485fe93f3ecf0cd5222337d67f862
Eric Williams <ericwill@redhat.com>
2021-03-30 07:55:04 -0400

    Adapt make-release.sh to use the eclipse-che org (#360)
    Part of eclipse/che#19388
    
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -7,7 +7,7 @@
 TRIGGER_RELEASE=0 
 NOCOMMIT=0
 TMP=""
-REPO=git@github.com:eclipse/che-devfile-registry
+REPO=git@github.com:eclipse-che/che-devfile-registry
 REGISTRY=quay.io
 ORGANIZATION=eclipse
 
