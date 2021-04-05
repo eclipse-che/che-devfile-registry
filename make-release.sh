3119eab220f504f65e2a1ff0ae681465bbe84480
Mykhailo Kuznietsov <mkuznets@redhat.com>
2021-04-05 14:10:06 +0300

    Fall back to single-arch build

--- make-release.sh
+++ make-release.sh
@@ -43,7 +43,11 @@ performRelease()
   VERSION=$(head -n 1 VERSION)
   SHORT_SHA1=$(git rev-parse --short HEAD)
   DOCKERFILE_PATH=./build/dockerfiles/Dockerfile
-  docker buildx build --push --platform "${PLATFORMS}" --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}" --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA1}" -f ${DOCKERFILE_PATH} --build-arg PATCHED_IMAGES_TAG="${VERSION}" --target registry .
+  docker build -t ${IMAGE} -f ${DOCKERFILE_PATH} --build-arg PATCHED_IMAGES_TAG="${VERSION}" --target registry .
+  docker tag ${IMAGE} "quay.io/eclipse/${IMAGE}:${SHORT_SHA1}"
+  docker push "quay.io/eclipse/${IMAGE}:${SHORT_SHA1}"
+  docker tag ${IMAGE} "quay.io/eclipse/${IMAGE}:${VERSION}"
+  docker push "quay.io/eclipse/${IMAGE}:${VERSION}"
 
   set +xe
 }
