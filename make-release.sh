d10611aeee82e7637fe3ee0522daeb1d3666df5c
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-04-28 18:36:46 +0530

    Resolves merge conflict
    Moved verification which checks if sidecar images are released before devfile registry image to verify_containers.sh script. And same is being executed through release workflow.

--- make-release.sh
+++ make-release.sh
@@ -9,8 +9,6 @@ NOCOMMIT=0
 TMP=""
 REPO=git@github.com:eclipse-che/che-devfile-registry
 
-SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-t'|'--tag-release') TAG_RELEASE=1; NOCOMMIT=0; shift 0;;
@@ -25,84 +23,6 @@ usage ()
 {
   echo "Usage: $0  --version [VERSION TO RELEASE] [--tag-release]"
   echo "Example: $0 --version 7.27.0 --tag-release"; echo
-}
-
-verifyContainerExistsWithTimeout()
-{
-    this_containerURL=$1
-    this_timeout=$2
-    containerExists=0
-    count=1
-    (( timeout_intervals=this_timeout*3 ))
-    while [[ $count -le $timeout_intervals ]]; do # echo $count
-        echo "       [$count/$timeout_intervals] Verify ${1} exists..." 
-        # check if the container exists
-        verifyContainerExists "$1"
-        if [[ ${containerExists} -eq 1 ]]; then break; fi
-        (( count=count+1 ))
-        sleep 20s
-    done
-    # or report an error
-    if [[ ${containerExists} -eq 0 ]]; then
-        echo "[ERROR] Did not find ${1} after ${this_timeout} minutes - script must exit!"
-        exit 1;
-    fi
-}
-
-verifyContainerExists()
-{
-    this_containerURL="${1}"
-    this_image=""; this_tag=""
-    this_image=${this_containerURL#*/}
-    this_tag=${this_image##*:}
-    this_image=${this_image%%:*}
-    this_url="https://quay.io/v2/${this_image}/manifests/${this_tag}"
-    # echo $this_url
-
-    # get result=tag if tag found, result="null" if not
-    result="$(curl -sSL "${this_url}"  -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json" 2>&1 || true)"
-    if [[ $(echo "$result" | jq -r '.schemaVersion' || true) == "1" ]] && [[ $(echo "$result" | jq -r '.tag' || true) == "$this_tag" ]]; then
-        echo "[INFO] Found ${this_containerURL} (tag = $this_tag)"
-        containerExists=1
-    elif [[ $(echo "$result" | jq -r '.schemaVersion' || true) == "2" ]]; then
-        arches=$(echo "$result" | jq -r '.manifests[].platform.architecture')
-        if [[ $arches ]]; then
-            echo "[INFO] Found ${this_containerURL} (arches = $arches)"
-        fi
-        containerExists=1
-    else
-        # echo "[INFO] Did not find ${this_containerURL}"
-        containerExists=0
-    fi
-}
-
-checkRequiredImagesExist()
-{
-  IMAGE_QUAY_PREFIX="quay.io/eclipse"
-  TAG=$(head -n 1 VERSION)
-  while read -r line; do
-    IMAGE_NAME=$(echo "$line" | tr -s ' ' | cut -f 1 -d ' ')
-    # echo "Checking ${IMAGE_QUAY_PREFIX}/${IMAGE_NAME}:${TAG} ..."
-    verifyContainerExistsWithTimeout "${IMAGE_QUAY_PREFIX}/${IMAGE_NAME}:${TAG}" 1 &
-  done < "${SCRIPT_DIR}"/arbitrary-users-patch/base_images
-  wait
-}
-
-buildAndPushImages() 
-{
-  set -xe
-
-  #Build and push patched base images and happy path image
-  TAG=$(head -n 1 VERSION)
-  export TAG
-  # Build and push base images
-  ./arbitrary-users-patch/build_images.sh --push --rm 
-  # Build and push happy path image, which depends on the above
-  ./arbitrary-users-patch/happy-path/build_happy_path_image.sh --push --rm
-  
-  checkRequiredImagesExist
-
-  set +xe
 }
 
 if [[ ! ${VERSION} ]]; then
@@ -191,10 +111,6 @@ updateVersionFile "${VERSION}"
 commitChangeOrCreatePR "${VERSION}" "${BRANCH}" "pr-${BRANCH}-to-${VERSION}"
 
 if [[ $TAG_RELEASE -eq 1 ]]; then
-  # push new branch to release branch to trigger CI build
-  fetchAndCheckout "${BRANCH}"
-  buildAndPushImages
-
   # tag the release
   git checkout "${BRANCH}"
   git tag "${VERSION}"
