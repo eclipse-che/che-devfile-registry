e97138b4f1e1b8a9f7af922fad7df7037a4509af
Mykhailo Kuznietsov <mkuznets@redhat.com>
2021-04-29 19:05:37 +0300

    fixup! Add workflow for checking sidecar digests

--- arbitrary-users-patch/check_sidecar_image_digests.sh
+++ arbitrary-users-patch/check_sidecar_image_digests.sh
@@ -7,6 +7,10 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
+# Check for new digests for all base images in base_images file
+# If found, a pull request will be created with updated digests.
+# This script requires GITHUB_TOKEN in order for Hub to be able to create a PR,
+# as well as configured Git name & email configuration
 
 set -e
 
@@ -39,7 +43,7 @@ createPR() {
     set -e
 }
 
-BUILT_IMAGES=""
+cp "${SCRIPT_DIR}"/base_images "${SCRIPT_DIR}"/base_images.copy
 while read -r line; do
   dev_container_name=$(echo "$line" | tr -s ' ' | cut -f 1 -d ' ')
   base_image_name=$(echo "$line" | tr -s ' ' | cut -f 2 -d ' ')
@@ -48,13 +52,14 @@ while read -r line; do
   latest_digest="$(skopeo inspect --tls-verify=false docker://"${base_image_name}" 2>/dev/null | jq -r '.Digest')"
   echo "latest digest ---> ${latest_digest}"
   latest_digest="${base_image_name%:*}@${latest_digest}"
-  if [[ ${latest_digest} != ${base_image_digest} ]]; then
+  if [[ "${latest_digest}" != "${base_image_digest}" ]]; then
     echo "[INFO] Detected newer image digest for ${base_image_name}"
-    sed -i "s|${base_image_digest}$|${latest_digest}|" "${SCRIPT_DIR}"/base_images
+    sed -i "s|${base_image_digest}$|${latest_digest}|" "${SCRIPT_DIR}"/base_images.copy
   else
     echo "[INFO] Image ${base_image_name} has valid digest"
   fi
 done < "${SCRIPT_DIR}"/base_images
+mv "${SCRIPT_DIR}"/base_images  "${SCRIPT_DIR}"/base_images.copy
 
 set +e
 hasChanges=$(git diff --exit-code "${SCRIPT_DIR}"/base_images)
