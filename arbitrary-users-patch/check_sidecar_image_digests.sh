1c84f5530d17d863a1f2df16265dec81ba8af93e
Mykhailo Kuznietsov <mkuznets@redhat.com>
2021-04-30 17:30:54 +0300

    fixup! fixup! fixup! Add NO_OP mode

--- arbitrary-users-patch/check_sidecar_image_digests.sh
+++ arbitrary-users-patch/check_sidecar_image_digests.sh
@@ -51,29 +51,28 @@ createPR() {
     set -e
 }
 
-cp "${SCRIPT_DIR}"/base_images "${SCRIPT_DIR}"/base_images.copy
-while read -r line; do
-  dev_container_name=$(echo "$line" | tr -s ' ' | cut -f 1 -d ' ')
-  base_image_name=$(echo "$line" | tr -s ' ' | cut -f 2 -d ' ')
-  base_image_digest=$(echo "$line" | tr -s ' ' | cut -f 3 -d ' ' )
-  echo "Checking ${NAME_FORMAT}/${dev_container_name} based on $base_image_name ..."
-  latest_digest="$(skopeo inspect --tls-verify=false docker://"${base_image_name}" 2>/dev/null | jq -r '.Digest')"
-  echo "latest digest ---> ${latest_digest}"
-  latest_digest="${base_image_name%:*}@${latest_digest}"
-  if [[ "${latest_digest}" != "${base_image_digest}" ]]; then
-    echo "[INFO] Detected newer image digest for ${base_image_name}"
-    sed -i "s|${base_image_digest}$|${latest_digest}|" "${SCRIPT_DIR}"/base_images.copy
-  else
-    echo "[INFO] Image ${base_image_name} has valid digest"
-  fi
-done < "${SCRIPT_DIR}"/base_images
-mv "${SCRIPT_DIR}"/base_images.copy  "${SCRIPT_DIR}"/base_images
+# cp "${SCRIPT_DIR}"/base_images "${SCRIPT_DIR}"/base_images.copy
+# while read -r line; do
+#   dev_container_name=$(echo "$line" | tr -s ' ' | cut -f 1 -d ' ')
+#   base_image_name=$(echo "$line" | tr -s ' ' | cut -f 2 -d ' ')
+#   base_image_digest=$(echo "$line" | tr -s ' ' | cut -f 3 -d ' ' )
+#   echo "Checking ${NAME_FORMAT}/${dev_container_name} based on $base_image_name ..."
+#   latest_digest="$(skopeo inspect --tls-verify=false docker://"${base_image_name}" 2>/dev/null | jq -r '.Digest')"
+#   echo "latest digest ---> ${latest_digest}"
+#   latest_digest="${base_image_name%:*}@${latest_digest}"
+#   if [[ "${latest_digest}" != "${base_image_digest}" ]]; then
+#     echo "[INFO] Detected newer image digest for ${base_image_name}"
+#     sed -i "s|${base_image_digest}$|${latest_digest}|" "${SCRIPT_DIR}"/base_images.copy
+#   else
+#     echo "[INFO] Image ${base_image_name} has valid digest"
+#   fi
+# done < "${SCRIPT_DIR}"/base_images
+# mv "${SCRIPT_DIR}"/base_images.copy  "${SCRIPT_DIR}"/base_images
 
 set +e
-hasChanges=$(git diff --exit-code "${SCRIPT_DIR}"/base_images)
-if [[ ${hasChanges} -eq 1 ]]; then
+if [[ $(git diff --exit-code "${SCRIPT_DIR}"/base_images) ]]; then
   if [[ ${NO_OP} == "true" ]]; then
-    echo "[INFO] Changes detected, see base_images file changes:\n"
+    echo "[INFO] Changes detected, see base_images file changes:"
     git diff --exit-code "${SCRIPT_DIR}"/base_images
   else
     echo "[INFO] Changes detected, generating PR with new digests"
