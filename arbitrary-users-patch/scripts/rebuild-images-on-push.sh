4b139043e2c4c3c090f7a45efa176d231d0ecb1c
Vitaliy Gulyy <vgulyy@redhat.com>
2021-09-27 16:25:14 +0300

    chore(base images): rebuildimages when pushing to ./dockerfiles/
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- arbitrary-users-patch/scripts/rebuild-images-on-push.sh
+++ arbitrary-users-patch/scripts/rebuild-images-on-push.sh
@@ -27,7 +27,8 @@ ROOT_DIR=$(cd ${BASE_DIR}/../..; pwd)
 
 cd "${ROOT_DIR}"
 
-SHORT_SHA1=$(git rev-parse --short HEAD)
+# SHORT_SHA1=$(git rev-parse --short HEAD)
+SHORT_SHA1=$(git log -n1 --format="%h")
 echo "COMMIT SHA: ${SHORT_SHA1}"
 echo "> changes in -------------------------------------------------------"
 CHANGES=$(git show --pretty="format:" --name-only ${SHORT_SHA1})
