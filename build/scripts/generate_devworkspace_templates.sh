987fa643a6f74e5f48141818331d9a1450b47051
Igor Vinokur <ivinokur@redhat.com>
2021-11-22 10:58:55 +0200

    chore: Pre-build DevWorkspace templates for devfile v2
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

---
+++ build/scripts/generate_devworkspace_templates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+mkdir /build/out/
+for dir in /build/devfiles/*/
+do
+  devfile=$(cat ${dir}meta.yaml | yq -r '."links"."v2"')
+  if [ $devfile != null ]; then
+    dir=${dir%/}
+    dir=/build/out/${dir##*/}
+    mkdir ${dir}
+    npx @eclipse-che/che-theia-devworkspace-handler --devfile-url:$devfile --output-file:${dir}/meta.yaml
+  fi
+done
