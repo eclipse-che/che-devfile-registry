ac70c9d0f416d8f09f2a7c6da9b565ce2bdf9754
Nick Boldt <nboldt@redhat.com>
2022-11-24 04:10:34 -0500

    chore: theia/next is dead, remove it (part 2) (#687)
    Change-Id: I7e4f91dd5350a37850e26013e8c623f2039460cd
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- build/dockerfiles/entrypoint.sh
+++ build/dockerfiles/entrypoint.sh
@@ -147,7 +147,7 @@ if [ -n "$PUBLIC_URL" ]; then
   PUBLIC_URL=${PUBLIC_URL%/}
   sed -i "s|{{ DEVFILE_REGISTRY_URL }}|${PUBLIC_URL}|" "${devfiles[@]}" "${metas[@]}" "${templates[@]}" "$INDEX_JSON"
 
-  # Add PUBLIC_URL at the begining of 'icon' field and links ('self', 'eclipse/che-theia/latest' and 'eclipse/che-theia/next')
+  # Add PUBLIC_URL at the begining of 'icon' field and links ('self', 'eclipse/che-theia/latest')
   sed -i "s|\"icon\": \"/images/|\"icon\": \"${PUBLIC_URL}/images/|" "$INDEX_JSON"
   sed -i "s|\"self\": \"/devfiles/|\"self\": \"${PUBLIC_URL}/devfiles/|" "$INDEX_JSON"
   sed -i "s|\"eclipse/che-theia/latest\": \"/devfiles/|\"eclipse/che-theia/latest\": \"${PUBLIC_URL}/devfiles/|" "$INDEX_JSON"
