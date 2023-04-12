ccfe08630f23f6ec0e694696b393302a4d86f02c
Igor Vinokur <ivinokur@redhat.com>
2023-04-12 11:15:06 +0300

    Add Github Enterprise repositories support
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

--- tools/devworkspace-generator/src/github/github-resolver.ts
+++ tools/devworkspace-generator/src/github/github-resolver.ts
@@ -18,18 +18,20 @@ import { injectable } from 'inversify';
 export class GithubResolver {
   // eslint-disable-next-line max-len
   static readonly GITHUB_URL_PATTERN =
-    /^(?:http)(?:s)?(?:\:\/\/)github\.com\/(?<repoUser>[^\/]+)\/(?<repoName>[^\/]+)((\/)|(?:\/(blob|tree)\/(?<branchName>[^\/]+)(?:\/(?<subFolder>.*))?))?$/;
+    /^(?<scheme>https?):\/\/(?<host>github(\..+)?\.[^\/]+)\/(?<repoUser>[^\/]+)\/(?<repoName>[^\/]+)((\/)|\/(blob|tree)\/(?<branchName>[^\/]+)(?:\/(?<subFolder>.*))?)?$/;
 
   resolve(link: string): GithubUrl {
     const match = GithubResolver.GITHUB_URL_PATTERN.exec(link);
     if (!match) {
       throw new Error(`Invalid github URL: ${link}`);
     }
+    const scheme = this.getGroup(match, 'scheme');
+    const hostName = this.getGroup(match, 'host');
     const repoUser = this.getGroup(match, 'repoUser');
     const repoName = this.getGroup(match, 'repoName');
     const branchName = this.getGroup(match, 'branchName', 'HEAD');
     const subFolder = this.getGroup(match, 'subFolder');
-    return new GithubUrl(repoUser, repoName, branchName, subFolder);
+    return new GithubUrl(scheme, hostName, repoUser, repoName, branchName, subFolder);
   }
 
   getGroup(match: RegExpExecArray, groupName: string, defaultValue?: string) {
