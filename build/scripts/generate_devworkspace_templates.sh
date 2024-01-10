db0288ca52dfc70cb896a8f8dfe72f0e74d12608
Mykhailo Kuznietsov <mkuznets@redhat.com>
2024-01-10 16:31:19 +0000

    chore: release: bump to 7.80.0 in 7.80.x
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- build/scripts/generate_devworkspace_templates.sh
+++ build/scripts/generate_devworkspace_templates.sh
@@ -15,7 +15,7 @@ if [[ -z "$VERSION" || "$VERSION" == *"-next" ]]; then
   VERSION="main"
 fi
 
-CHE_DEVWORKSPACE_GENERATOR_VERSION=next
+CHE_DEVWORKSPACE_GENERATOR_VERSION=7.80.0
 PLUGIN_REGISTRY_URL=https://eclipse-che.github.io/che-plugin-registry/${VERSION}/v3
 
 for dir in /build/devfiles/*/
